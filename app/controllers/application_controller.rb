f4abff8fa4f2c75d0312a7b9fbb910f022e92b8d
fazal karim <meetfazalkarim@gmail.com>
2019-09-12 19:32:37 +0500

    configured devise and bootstrap

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+    before_action :authenticate_user!
+
+    protect_from_forgery prepend: true
+
+    def landing_page
+        render 'pages/landing_page'
+    end
 end
